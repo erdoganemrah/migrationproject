f14def048396b8910ab111670c77376a231b7d18
Emrah Erdoğan <emraherdogan@outlook.com.tr>
2021-10-04 15:09:04 +0300

    Update migration-steps.sh

--- migration-steps.sh
+++ migration-steps.sh
@@ -1,10 +1,10 @@
 git clone https://github.com/dalibo/sqlserver2pgsql.git
 cd sqlserver2pgsql
-vim sqlserver2pgsql.pl
 
-hierarchyid => bytea
-geography => bytea
-geomerty => bytea
+vim sqlserver2pgsql.pl
+  'hierarchyid' => 'bytea',
+  'geography' => 'bytea',
+  'geomerty' => 'bytea'
 
 sed -i 's/\[dbo\]\.\[AccountNumber\]/[nvarchar](15)/g' tables.sql
 sed -i 's/\[dbo\]\.\[Flag\]/[bit]/g' tables.sql
